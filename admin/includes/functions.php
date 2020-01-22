69e44bfd148ec4ca837a4959335620c075f48eea
kdenno <kabuyedennis3@gmail.com>
2020-01-22 12:08:50 +0300

    insert admin posts query

--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -1,4 +1,12 @@
 <?php
+function confirm_query($result) {
+  global $connection;
+  if(!$result) {
+    die('QUERY FAILED '. mysqli_error($connection));
+
+  }
+
+}
 function insert_categories()
 {
   global $connection;
